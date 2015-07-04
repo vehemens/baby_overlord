afe848bbadcbf6562907a74f2bea590a20dd8064
vehemens <vehemens@verizon.net>
2015-07-04 01:03:23 -0700

    Add nibro timer fix

--- main_controller/Linux/build/LinuxMotionTimer.cpp
+++ main_controller/Linux/build/LinuxMotionTimer.cpp
@@ -10,6 +10,51 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#if defined(__i386__)
+#define __NR_ioprio_set         289
+#define __NR_ioprio_get         290
+#elif defined(__ppc__)
+#define __NR_ioprio_set         273
+#define __NR_ioprio_get         274
+#elif defined(__x86_64__)
+#define __NR_ioprio_set         251
+#define __NR_ioprio_get         252
+#elif defined(__ia64__)
+#define __NR_ioprio_set         1274
+#define __NR_ioprio_get         1275
+#else
+#error "Unsupported arch"
+#endif
+
+static inline int ioprio_set(int which, int who, int ioprio)
+{
+        return syscall(__NR_ioprio_set, which, who, ioprio);
+}
+
+static inline int ioprio_get(int which, int who)
+{
+        return syscall(__NR_ioprio_get, which, who);
+}
+
+enum {
+        IOPRIO_CLASS_NONE,
+        IOPRIO_CLASS_RT,
+        IOPRIO_CLASS_BE,
+        IOPRIO_CLASS_IDLE,
+};
+
+enum {
+        IOPRIO_WHO_PROCESS = 1,
+        IOPRIO_WHO_PGRP,
+        IOPRIO_WHO_USER,
+};
+
+#define IOPRIO_CLASS_SHIFT      13
+
+
 
 using namespace Robot;
 
@@ -23,16 +68,26 @@ LinuxMotionTimer::LinuxMotionTimer(MotionManager* manager)
 void *LinuxMotionTimer::TimerProc(void *param)
 {
     LinuxMotionTimer *timer = (LinuxMotionTimer *)param;
-    static struct timespec next_time;
+    struct timespec next_time;
+    struct timespec current_time;
     clock_gettime(CLOCK_MONOTONIC,&next_time);
+    // Set I/O priority to realtime
+    ioprio_set(IOPRIO_WHO_PROCESS, getpid(), (IOPRIO_CLASS_RT << 13) | 0);
 
     while(!timer->m_FinishTimer)
     {
-        next_time.tv_sec += (next_time.tv_nsec + MotionModule::TIME_UNIT * 1000000) / 1000000000;
-        next_time.tv_nsec = (next_time.tv_nsec + MotionModule::TIME_UNIT * 1000000) % 1000000000;
-
         if(timer->m_Manager != NULL)
             timer->m_Manager->Process();
+
+        // Calculate the next reachable period
+        clock_gettime(CLOCK_MONOTONIC, &current_time);
+	do
+        {
+            next_time.tv_sec += (next_time.tv_nsec + MotionModule::TIME_UNIT * 1000000) / 1000000000;
+            next_time.tv_nsec = (next_time.tv_nsec + MotionModule::TIME_UNIT * 1000000) % 1000000000;
+        }
+        while(current_time.tv_sec > next_time.tv_sec
+            || (current_time.tv_sec == next_time.tv_sec && current_time.tv_nsec > next_time.tv_nsec));
 
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next_time, NULL);
     }
